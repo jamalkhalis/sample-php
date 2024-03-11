50535df3bd6c0a62eaed399cbe3e1c1ed6c31743
Jamal KHALIS <39938150+jamalkhalis@users.noreply.github.com>
2024-03-11 16:42:32 +0000

    update

--- assets/php/email.php
+++ assets/php/email.php
@@ -1,5 +1,5 @@
 <?php
 
-	$recipient = "bmotivated52@gmail.com"; // Note: Please replace this email with your recipient's email
+	$recipient = "im.mathematician.lover@gmail.com"; // Note: Please replace this email with your recipient's email
 
 ?>
