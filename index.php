a1de8f4ff2871d5c7c5bce9b52e21bde12061640
Jamal KHALIS <39938150+jamalkhalis@users.noreply.github.com>
2024-03-11 16:44:41 +0000

    update

--- index.php
+++ index.php
@@ -1,22 +1,3 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-
-use Cowsayphp\Farm;
-
-header('Content-Type: text/plain');
-
-$text = "Set a message by adding ?message=<message here> to the URL";
-if(isset($_GET['message']) && $_GET['message'] != '') {
-	$text = htmlspecialchars($_GET['message']);
-}
-
-$cow = Farm::create(\Cowsayphp\Farm\Cow::class);
-echo $cow->say($text);
-
-// Start HTML output
-?>
-
-
 <!doctype html>
 <html lang="en" dir="ltr" data-bs-theme="auto">
 <head>
